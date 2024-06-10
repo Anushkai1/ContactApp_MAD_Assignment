a48925ed290a8289eacce0d3637a907c9a21dab8
Anushka Isuru <anushka8.ai@gmail.com>
2024-06-11 00:20:45 +0530

    Back Button from Show More to Contact List

--- app/src/main/java/com/example/contactapp/MainActivity2.java
+++ app/src/main/java/com/example/contactapp/MainActivity2.java
@@ -1,6 +1,9 @@
 package com.example.contactapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -30,5 +33,14 @@ TextView textView;
         textView.setText(getIntent().getStringExtra("email"));
         textView = findViewById(R.id.address);
         textView.setText(getIntent().getStringExtra("address"));
+
+        Button back = findViewById(R.id.button);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
