d10fedefeaebbed644a340db9e57c5194b6265d3
Anushka Isuru <anushka8.ai@gmail.com>
2024-06-11 00:19:22 +0530

    Get Data in Show more come from Contact List

--- app/src/main/java/com/example/contactapp/MainActivity2.java
+++ app/src/main/java/com/example/contactapp/MainActivity2.java
@@ -1,6 +1,7 @@
 package com.example.contactapp;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +10,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
-
+TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +21,14 @@ public class MainActivity2 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        textView = findViewById(R.id.name);
+        textView.setText(getIntent().getStringExtra("name"));
+        textView = findViewById(R.id.phone);
+        textView.setText(getIntent().getStringExtra("phone"));
+        textView = findViewById(R.id.email);
+        textView.setText(getIntent().getStringExtra("email"));
+        textView = findViewById(R.id.address);
+        textView.setText(getIntent().getStringExtra("address"));
     }
 }
